a12c3cafa7c674ff4404492bd5e5b9850f75e25d
Cycle Five <cycle.five@proton.me>
2024-02-09 16:02:24 -0500

    Trying to get this working

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use fiche_rs::{Args, FicheSettings};
 
 use clap::Parser;
 
-#[cfg(not(shuttle))]
+#[cfg(shuttle_rocket)]
+use {
+    rocket::fs::relative,
+    rocket::fs::NamedFile,
+    std::path::{Path, PathBuf},
+};
+
+#[cfg(all(not(shuttle_rocket), not(shuttle_axum)))]
 #[cfg(not(tarpaulin_include))]
 /// The main function
 fn main() -> Result<(), String> {
@@ -16,7 +23,7 @@ fn main() -> Result<(), String> {
     fiche_run(settings)
 }
 
-#[cfg(shuttle)]
+#[cfg(shuttle_axum)]
 #[cfg(not(tarpaulin_include))]
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
@@ -25,4 +32,24 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .nest_service("/assets", ServeDir::new("assets"));
 
     Ok(router.into())
+}
+
+#[cfg(shuttle_rocket)]
+#[rocket::get("/<path..>")]
+pub async fn serve(mut path: PathBuf) -> Option<NamedFile> {
+    path.set_extension("html");
+    let mut path = Path::new(relative!("assets")).join(path);
+    if path.is_dir() {
+        path.push("index.html");
+    }
+
+    NamedFile::open(path).await.ok()
+}
+
+#[cfg(shuttle_rocket)]
+#[shuttle_runtime::main]
+async fn rocket() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build().mount("/", rocket::routes![serve]);
+
+    Ok(rocket.into())
 }
